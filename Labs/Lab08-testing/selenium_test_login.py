45d5767084cf077e0d4b3ff5b0860a920bb07665
N23DCPT047 <n23dcpt047@student.ptithcm.edu.vn>
2025-10-02 22:38:14 +0700

    Update selenium_test_login.py

--- Labs/Lab08-testing/selenium_test_login.py
+++ Labs/Lab08-testing/selenium_test_login.py
@@ -9,32 +9,19 @@ from webdriver_manager.chrome import ChromeDriverManager
 URL = "https://n23dcpt047.github.io/NhapMonCongNghePhanMem/"  
 
 @pytest.fixture
-def driver():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless=new")
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    yield driver
-    driver.quit()
-
 def test_login_success(driver):
     driver.get(URL)
-    driver.find_element(By.ID, "username").send_keys("admin")
-    driver.find_element(By.ID, "password").send_keys("1234")
+    driver.find_element(By.ID, "username").send_keys("admin@example.com")
+    driver.find_element(By.ID, "password").send_keys("123456")
     driver.find_element(By.ID, "btnLogin").click()
-    time.sleep(1)
-    
-    assert "Thành công" in driver.page_source.lower()
 
-def test_login_fail(driver):
-    driver.get(URL)
-    driver.find_element(By.ID, "username").send_keys("wrong")
-    driver.find_element(By.ID, "password").send_keys("wrong")
-    driver.find_element(By.ID, "btnLogin").click()
-    time.sleep(1)
-    assert "Sai" in driver.page_source.lower()
+    alert = driver.switch_to.alert
+    assert "Đăng nhập thành công" in alert.text
+    alert.accept()
+
 
 def test_login_empty(driver):
     driver.get(URL)
     driver.find_element(By.ID, "btnLogin").click()
-    time.sleep(1)
-    assert "Không được để trống" in driver.page_source.lower()
+    error_text = driver.find_element(By.ID, "errorMsg").text
+    assert "không được để trống" in error_text.lower()
